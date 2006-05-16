d76efd84132353ed838fe4e1afb99a65a88fe8c2
Mark Fasheh <mark.fasheh@oracle.com>
2006-05-16 01:24:43 +0000

    * add copyright and license to lvb_torture
    Original-revision: 21

--- programs/lvb_torture/lvb_torture.c
+++ programs/lvb_torture/lvb_torture.c
@@ -1,4 +1,22 @@
 /*
+ * Copyright (C) 2006 Oracle.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 021110-1307, USA.
+ */
+/*
  * My first MPI program. Ripped from write_append_truncate.c
  */
 #include <stdlib.h>
