55e20b33d43d439cb4139550c6c650cb272e4a8f
Marcos Matsunaga <marcos.matsunaga@oracle.com>
2007-02-23 23:20:42 +0000

    Fixed a small problem with sa_restorer that prevented to be built on 64 systems (ia64 and x86_64) and added copyright info.
    File(s) modified:
    
    M    lock_grab.c
    
    Signed-off-by: Sunil Mushran <sunil.mushran@oracle.com>
    
    Original-revision: 69

--- programs/lock_grab/lock_grab.c
+++ programs/lock_grab/lock_grab.c
@@ -1,3 +1,27 @@
+/*
+ * Copyright (C) 2006 Oracle.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 021110-1307, USA.
+ */
+/*
+ *
+ * Description : Will grab locks and hold them to add some stress to
+ *               the OCFS2 DLM.
+ * Author      : Joel Becker
+ */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -53,9 +77,8 @@ static int setup_signals()
 {
     int rc = 0;
     struct sigaction act;
-
+    memset(&act, 0, sizeof(act));
     act.sa_sigaction = NULL;
-    act.sa_restorer = NULL;
     sigemptyset(&act.sa_mask);
     act.sa_handler = handler;
 #ifdef SA_INTERRUPT
