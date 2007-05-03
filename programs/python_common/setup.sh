f28867ad657dfde36b40e84272740a5b1f2efd73
Marcos Matsunaga <marcos.matsunaga@oracle.com>
2007-05-03 18:02:02 +0000

    Made the scripts discover and configure the LAM/MPI installation directory. It now configures config.sh and config.py to use MPIDIR as location of the LAM/MPI binaries.
    Changed files:
    
    M    python_common/config_shell.skel
    M    python_common/setup.sh
    M    python_common/config_py.skel
    
    Signed-off-by: Sunil Mushran <sunil.mushran@oracle.com>
    
    Original-revision: 94

--- programs/python_common/setup.sh
+++ programs/python_common/setup.sh
@@ -1,9 +1,39 @@
 #!/bin/bash -x
 #
+# Copyright (C) 2006 Oracle.  All rights reserved.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 021110-1307, USA.
+
+#
 if [ $# -ne 1 ]; then
    echo -e 'Usage: $0 <DESTDIR>';
    exit 1;
 fi;
+LAMBOOT=`which lamboot`
+if [ -f ${LAMBOOT} ]; then
+   MPIDIR=`dirname ${LAMBOOT}`
+else
+   MPIDIR=/usr/bin
+fi;
 BINDIR=${1}/bin
 sed "s;<DESTDIR>;${1};g" ${BINDIR}/config_py.skel >  ${BINDIR}/config.py
+mv ${BINDIR}/config.py  ${BINDIR}/config_py.skel
+sed "s;<MPIDIR>;${MPIDIR};g" ${BINDIR}/config_py.skel >  ${BINDIR}/config.py
+#
 sed "s;<DESTDIR>;${1};g" ${BINDIR}/config_shell.skel >  ${BINDIR}/config.sh
+mv ${BINDIR}/config.sh ${BINDIR}/config_shell.skel
+sed "s;<MPIDIR>;${MPIDIR};g" ${BINDIR}/config_shell.skel >  ${BINDIR}/config.sh
+rm -f ${BINDIR}/config_py.skel ${MPIDIR}/config_shell.skel
