b1f38c45da234987525fb0fa99bd1940d6ece211
Marcos Matsunaga <marcos.matsunaga@oracle.com>
2007-06-28 16:44:12 +0000

    - 	Basically change it to control better the cycles. The cycles were running on buildkernel.py before and it now runs on run_buildkernel.py. - 	It also has a new option to initialize the directories each cycle (will remove the directory and extract the tar file). It also initialize all directories used every time run_buildkernel is executed (only once). -	It also runs quiet now, unless DEBUG=1 is set. -	New set of logfiles was created, untar and build will create its own logfiles (per PID). -	All spaces were replaced by tabs. -	Replace mpi_run_parts by lamexec.
    Changed files:
    
    M    buildkernel/buildkernel.py
    M    buildkernel/run_buildkernel.py
    
    Signed-off-by: Sunil Mushran <sunil.mushran@oracle.com>
    
    Original-revision: 108

--- programs/buildkernel/buildkernel.py
+++ programs/buildkernel/buildkernel.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 #
-# Copyright (C) 2006 Oracle.  All rights reserved.
+# Copyright (C) 2006 Oracle.	All rights reserved.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public
@@ -19,14 +19,14 @@
 # Boston, MA 021110-1307, USA.
 #
 # XXX: Future improvements:
-#   
-# Program     : buildkernel.py
+#	 
+# Program	  : buildkernel.py
 # Description : Run a build of the kernel on each one of the ocfs2
-#               partitions mounted, in parallel.
-#               i.e: The system has 7 partitions, it will run 7
-#                    builds in parallel, one for each partition.
-# Author      : Marcos E. Matsunaga 
-# E-mail      : Marcos.Matsunaga@oracle.com
+#					 partitions mounted, in parallel.
+#					 i.e: The system has 7 partitions, it will run 7
+#							builds in parallel, one for each partition.
+# Author		  : Marcos E. Matsunaga 
+# E-mail		  : Marcos.Matsunaga@oracle.com
 
 #
 import os, sys, time, optparse, socket, string, o2tf, pdb, timing, time, config
@@ -37,210 +37,258 @@ CHECKTHREAD_SLEEPTIME = 30
 #
 uname = os.uname()
 lhostname = str(socket.gethostname())
+logfile = config.LOGFILE
+nodelist = ''
+extract_tar = ''
 #
 # FUNCTIONS
 #
 def tbuild(thrid, command):
-   'tbuild spawn process and build a process list'
-   'tbuild takes 2 arguments:'
-   '   thrid - Thread number, which will index pidlist'
-   '   command - command to be executed'
-   if DEBUGON:
-      o2tf.printlog('buildkernel:tbuild - current directory %s' % \
-			os.getcwd(), 
+	'tbuild spawn process and build a process list'
+	'tbuild takes 2 arguments:'
+	'	 thrid - Thread number, which will index pidlist'
+	'	 command - command to be executed'
+	if DEBUGON:
+		o2tf.printlog('buildkernel:tbuild - current directory %s' % os.getcwd(), 
 			logfile, 
 			0, 
 			'')
-      o2tf.printlog('buildkernel:tbuild - command  %s ' % \
-			command, 
+		o2tf.printlog('buildkernel:tbuild - command	%s ' % command, 
 			logfile, 
 			0, 
 			'')
-   pidlist[thrid] = os.spawnv(os.P_NOWAIT, 
+	pidlist[thrid] = os.spawnv(os.P_NOWAIT, 
 			'/bin/bash', 
 			['bash', 
 			'-c', 
 			command])
-#   
-def check_thread(runnumber, logfile):
-   'check_thread checks the pidlist for running tasks until the last one \
-	completes.'
-   'check_thread takes 3 arguments:'
-   '   runnumer - Sequence number of a specific batch.'
-   '   logfile - logfile name'
-   o2tf.printlog('buildkernel:check_thread: Waiting for processes to finish \
-	on thread %s' % \
-			(runnumber), 
+#	 
+def check_thread(logfile):
+	'check_thread checks the pidlist for running tasks until the last one '
+	'completes.'
+	'check_thread takes 1 argument:'
+	'	 logfile - logfile name'
+	o2tf.printlog('buildkernel:check_thread: Waiting for processes to finish '
+		'on thread ',
+		logfile, 
+		0, 
+		'')
+	while len(pidlist) > 0:
+		o2tf.printlog('buildkernel:check_thread: Checking thread processes',
 			logfile, 
 			0, 
 			'')
-   while len(pidlist) > 0:
-      o2tf.printlog('buildkernel:check_thread: Checking thread processes' \
-			(runnumber), 
+		o2tf.printlog('buildkernel:check_thread: pid list %s' % (pidlist), 
 			logfile, 
 			0, 
 			'')
-      o2tf.printlog('buildkernel:check_thread: pid list %s' % 
-			(pidlist), 
-			logfile, 
-			0, 
-			'')
-      for z in range(len(pidlist)):
-           out = os.waitpid(pidlist[z],os.WNOHANG)
-           o2tf.printlog('buildkernel:check_thread: z=%s, out=%s' % \
-			(z, out[1]), 
-			logfile, 
-			0, 
-			'')
-           if out[0] > 0:
-              o2tf.printlog('buildkernel:check_thread: Removing pid \
-		%s from the list' %  \
-			pidlist[z], 
-			logfile, 
-			0, 
-			'')
-              t2 = time.time()
-              o2tf.printlog('buildkernel:check_thread: %s [%s]: Build \
-		time is %f seconds' % 
-			(str(socket.gethostname()), pidlist[z], t2 - t1), 
-			logfile, 
-			0, 
-			'')
-              pidlist.remove(out[0])
-              break
-      time.sleep(CHECKTHREAD_SLEEPTIME)
+		for z in range(len(pidlist)):
+			out = os.waitpid(pidlist[z],os.WNOHANG)
+			o2tf.printlog('buildkernel:check_thread: z=%s, out=%s' % (z, out[1]), 
+				logfile, 
+				0, 
+				'')
+			if out[0] > 0:
+				o2tf.printlog('buildkernel:check_thread: Removing pid '
+					'%s from the list' %	pidlist[z], 
+					logfile, 
+					0, 
+					'')
+				t2 = time.time()
+				o2tf.printlog('buildkernel:check_thread: %s [%s]: Build '
+					'time is %f seconds' % 
+					(str(socket.gethostname()), pidlist[z], t2 - t1), 
+					logfile, 
+					0, 
+					'')
+				pidlist.remove(out[0])
+				break
+		time.sleep(CHECKTHREAD_SLEEPTIME)
 #
 # MAIN
 #
-Usage = 'Usage: %prog [-c|--count count] \
+Usage = 'Usage: %prog [-c|--cleardir] \
 [-d|--dirlist dirlist] \
+[-e|--extract] \
 [-l|-logfile logfilename] \
 [-n | --nodelist nodename] \
 [-t|--tarfile fullpath tar filename] \
 [-h|--help]'
 if __name__=='__main__':
-    parser = optparse.OptionParser(Usage)
+	parser = optparse.OptionParser(Usage, conflict_handler="resolve")
+#
+	parser.add_option('-c', 
+		'--cleardir', 
+		action="store_true",
+		dest='cleardir',
+		default=False,
+		help='Clear the node directories on the partitions. Default=False.')
+#
+	parser.add_option('-d', 
+		'--directorylist', 
+		dest='dirlist',
+		type='string',
+		help='List of directories that will be used by the test. ')
+#
+	parser.add_option('-D',
+		'--debug', 
+		action="store_true",
+		dest='debug',
+		default=False,
+		help='Turn the debug option on. Default=False.')
+#
+	parser.add_option('-e',
+		'--extract_tar', 
+		action="store_true",
+		dest='extract_tar',
+		default=False,
+		help='Extract Linux tree kernel. Default=False.')
+#
+	parser.add_option('-l', 
+		'--logfile', 
+		dest='logfile',
+		type='string', 
+		help='If logfile is specified, a single logfile will \
+			be used by all processes, otherwise, \
+			individual logfiles will be created per \
+			process. Default will be to create a logfile \
+			per process.')
+#
+	parser.add_option('-n', 
+		'--nodelist', 
+		dest='nodelist',
+		type='string', 
+		help='Nodename owner of the directory it will run the \
+			find command. Default to local hostname.')
+#
+	parser.add_option('-t',
+		'--tarfile',
+		dest='tarfile',
+		type='string',
+		help='Fullpath filename of the tar file containing \
+			the kernel that will be used.')
 
-    parser.add_option('-c', 
-			'--count', 
-			dest='count', 
-			default='1', 
-			type='int', 
-			help='Number of times it will build \
-				the kernel. Default = 1.')
 #
-    parser.add_option('-d', 
-			'--directorylist', 
-			dest='dirlist',
-			type='string',
-			help='List of directories that will be \
-				used by the test. ')
+	(options, args) = parser.parse_args()
+	if len(args) != 0:
+		parser.error('incorrect number of arguments')
 #
-    parser.add_option('-l', 
-			'--logfile', 
-			dest='logfile',
-			type='string', 
-			help='If logfile is specified, a single logfile will \
-				be used by all processes, otherwise, \
-				individual logfiles will be created per \
-				process. Default will be to create a logfile \
-				per process.')
-#
-    parser.add_option('-n', 
-			'--nodelist', 
-			dest='nodelist',
-			type='string', 
-			help='Nodename owner of the directory it will run the \
-				find command. Default to local hostname.')
-#
-    parser.add_option('-t',
-                        '--tarfile',
-                        dest='tarfile',
-                        type='string',
-                        help='Fullpath filename of the tar file containing \
-                                the kernel that will be used.')
-
-#
-    (options, args) = parser.parse_args()
-    if len(args) != 0:
-        parser.error('incorrect number of arguments')
-#
-    if not options.tarfile:
-        parser.error('Must provide a gzipped kernel tarball to run the test.')
-    tarfile = options.tarfile
-    dirlist = options.dirlist.split(',')
-    dirlen = len(dirlist)
-    count = options.count
-    logfile = options.logfile
-    nodelen = len(options.nodelist)
-    if nodelen == 1:
-       nodelist = nodelist.add(options.nodelist)
-    else:
-       nodelist = options.nodelist.split(',')
-    nodelen = len(nodelist)
+	if options.debug:
+		DEBUGON=1
+	cleardir = options.cleardir
+	extract_tar = options.extract_tar
+	if options.extract_tar:
+		if not options.tarfile:
+			parser.error('Must provide a gzipped kernel tarball to run the test.')
+		else:
+			tarfile = options.tarfile
+	if not options.dirlist:
+		parser.error('Must provide a directory list where the test will run.')
+	dirlist = options.dirlist.split(',')
+	dirlen = len(dirlist)
+	if options.logfile:
+		logfile = options.logfile
+	if not options.cleardir and not options.extract_tar:
+		if not options.nodelist:
+			parser.error('Must provide a list of nodes where the test will run.')
+		nodelen = len(options.nodelist)
+		if nodelen == 1:
+			nodelist = nodelist.add(options.nodelist)
+		else:
+			nodelist = options.nodelist.split(',')
+			nodelen = len(nodelist)
 #
 if DEBUGON:
-   o2tf.printlog('buildkernel: dirlist = (%s)' % dirlist, logfile, 0, '')
-   o2tf.printlog('buildkernel: dirlen = (%s)' % dirlen, logfile, 0, '')
-   o2tf.printlog('buildkernel: logfile = (%s)' % logfile, logfile, 0, '')
-   o2tf.printlog('buildkernel: nodelist = (%s)' % nodelist, logfile, 0, '')
-   o2tf.printlog('buildkernel: count = (%s)' % count, logfile, 0, '')
-   o2tf.printlog('buildkernel: tarfile = (%s)' % tarfile, logfile, 0, '')
+	o2tf.printlog('buildkernel: cleardir = (%s)' % cleardir, 
+		logfile, 
+		0, 
+		'')
+	o2tf.printlog('buildkernel: dirlist = (%s)' % dirlist, 
+		logfile, 
+		0, 
+		'')
+	o2tf.printlog('buildkernel: dirlen = (%s)' % dirlen, 
+		logfile, 
+		0, 
+		'')
+	o2tf.printlog('buildkernel: logfile = (%s)' % logfile, 
+		logfile, 
+		0, 
+		'')
+	o2tf.printlog('buildkernel: nodelist = (%s)' % nodelist, 
+		logfile, 
+		0, 
+		'')
+	if options.extract_tar:
+		o2tf.printlog('buildkernel: tarfile = (%s)' % tarfile, 
+			logfile, 
+			0, 
+			'')
+	o2tf.printlog('buildkernel: extract_tar = (%s)' % extract_tar, 
+		logfile, 
+		0, 
+		'')
 #
-o2tf.extract_tar(DEBUGON, logfile, lhostname, options.dirlist, tarfile)
+if cleardir:
+	o2tf.ClearDir(DEBUGON, logfile, options.dirlist)
+	sys.exit()
+else:
+	if extract_tar:
+		o2tf.extract_tar(DEBUGON, logfile, options.dirlist, options.tarfile)
+		sys.exit()
 #
 # For each entry in dirlist, fork a process to run the build and a find on the
 # directory owned by another node.
 #
-# First find which node to run the find.
+# First find which node to run the find. This is the only reason a nodelist
+# is passed in the arguments list.
 #
 for y in range(nodelen):
-    if lhostname == nodelist[y]:
-       if y == 0:
-          nodefind = nodelist[ -1 ]
-       else:
-          nodefind = nodelist[ y - 1 ]
-       if DEBUGON:
-          o2tf.printlog('buildkernel:Main - Node to use on find %s' % \
-			nodefind, 
-			logfile, 
-			0, 
-			'')
+	if lhostname == nodelist[y]:
+		if y == 0:
+			nodefind = nodelist[ -1 ]
+		else:
+			nodefind = nodelist[ y - 1 ]
+		if DEBUGON:
+			o2tf.printlog('buildkernel:Main - Node to use on find %s' % nodefind, 
+				logfile, 
+				0, 
+				'')
 
 #
-for i in range(int(count)):
-    pidlist = [0] * dirlen * 2
-    for x in range(dirlen):
-       wdir = dirlist[x] + '/' + str(socket.gethostname()) +'/'+ config.KERNELDIR
-       cmd = 'cd ' + wdir + ';  make mrproper; make defconfig; /usr/bin/make -j2 V=1 2>&1 >> %s' % logfile
-
-       if DEBUGON:
-          o2tf.printlog('buildkernel:Main - current directory %s' % \
-			os.getcwd(), \
-			logfile, \
-			0, \
+import os.path
+logdir = os.path.dirname(logfile)
+pidlist = [0] * dirlen * 2
+for x in range(dirlen):
+	buildlog = logdir + '/build_' + lhostname + '_' + str(x) + '.log'
+	findlog = logdir + '/find_' + lhostname + '_' + str(x) + '.log'
+	wdir = dirlist[x] + '/' + str(socket.gethostname()) +'/'+ config.KERNELDIR
+	cmd = 'cd ' + wdir + ';  make mrproper 2>&1 1>> %s; \
+			make defconfig 2>&1 1>> %s; /usr/bin/make -j2 V=1 2>&1 1>> %s' %  \
+			(buildlog, buildlog, buildlog)
+#
+	if DEBUGON:
+		o2tf.printlog('buildkernel:Main - current directory %s' % os.getcwd(),
+			logfile,
+			0,
 			'')
-          o2tf.printlog('buildkernel:Main - working directory %s' % \
-			dirlist[x], \
-			logfile, \
-			0, \
+		o2tf.printlog('buildkernel:Main - working directory %s' % dirlist[x],
+			logfile,
+			0,
 			'')
-          o2tf.printlog('buildkernel:Main - wdir =  %s' % \
-			wdir, \
-			logfile, \
-			0, \
+		o2tf.printlog('buildkernel:Main - wdir =  %s' %  wdir,
+			logfile,
+			0,
 			'')
-          o2tf.printlog('buildkernel:Main - cmd = %s' % \
-			cmd, \
-			logfile, \
-			0, \
+		o2tf.printlog('buildkernel:Main - cmd = %s' % cmd,
+			logfile,
+			0,
 			'')
-       t1 = time.time()
-       tbuild(x, cmd)
-       cmd = 'cd ' + dirlist[x] + '/' + nodefind + '; find . -print \
-			(2>&1 >> %s' % logfile
-       tbuild(int(x + dirlen), cmd)
-       if DEBUGON:
-          o2tf.printlog('buildkernel:Main - cmd = %s' % cmd, logfile, 0, '')
-    check_thread(i, logfile)
-                                                                                                                      
+	t1 = time.time()
+	tbuild(x, cmd)
+	cmd = 'cd ' + dirlist[x] + '/' + nodefind + '; find . -print \
+		2>&1 1>> %s' % findlog
+	tbuild(int(x + dirlen), cmd)
+	if DEBUGON:
+		o2tf.printlog('buildkernel:Main - cmd = %s' % cmd, logfile, 0, '')
+check_thread(logfile)
--- programs/buildkernel/run_buildkernel.py
+++ programs/buildkernel/run_buildkernel.py
@@ -4,92 +4,141 @@ import os, sys, optparse, time, o2tf, pdb, config
 #
 DEBUGON = os.getenv('DEBUG',0)
 #
-Usage = 'Usage: %prog [-c|--count count] \
-[-n|--nodes nodelist] \
-[-t|--tarfile fullpath tar filename] \
-[-d|--directorylist dirlist] \
-[-l|-logfile logfilename] \
-[-u|--user username] \
-[-h|--help]'
+logfile = config.LOGFILE
 #
-if __name__=='__main__':
-    parser = optparse.OptionParser(Usage)
+def Initialize():
+	'Initialize the directories (remove and extract)'
 #
-    parser.add_option('-c', 
-			'--count', 
-			dest='count', 
-			default='1',
-			type='int', 
-			help='Number of times it will build the kernel. \
-				Default = 1.')
-#
-    parser.add_option('-u', 
-			'--user', 
-			dest='userid',
-			type='string', 
-			help='Userid used to open ssh connections.')
-#
-    parser.add_option('-d', 
-			'--directorylist', 
-			dest='dirlist',
-			type='string',
-			help='List of directories that will be used by the \
-				test.')
-#
-    parser.add_option('-l', 
-			'--logfile', 
-			dest='logfile',
-			default='%s/log/run_buildkernel.log' % config.O2TDIR,
-			type='string', 
-			help='If logfile is specified, a single logfile will \
-				be used by all processes, otherwise, \
-				individual logfiles will be created per \
-				process. Default will be to create a logfile \
-				per process.')
-#
-    parser.add_option('-n', 
-			'--nodes', 
-			dest='nodelist',
-			type='string',
-			help='List of nodes where the test will be executed.')
-#
-    parser.add_option('-t', 
-			'--tarfile', 
-			dest='tarfile', 
-			type='string', 
-			help='Fullpath filename of the tar file containing \
-				the kernel that will be used.')
-
-    (options, args) = parser.parse_args()
-    if len(args) != 0:
-        parser.error('incorrect number of arguments')
-    if not options.tarfile:
-        parser.error('Must provide a gzipped kernel tarball to run the test.')
-    dirlist = options.dirlist.split(',')
-    dirlen = len(dirlist)
-    nodelen = len(options.nodelist)
-    if nodelen == 1:
-       nodelist = nodelist.add(options.nodelist)
-    else:
-       nodelist = options.nodelist.split(',')
-    logfile = options.logfile
-    tarfile = options.tarfile
-buildcmd= config.BINDIR+'/buildkernel.py'
-if DEBUGON:
-   o2tf.printlog('dirlist = (%s)' % dirlist, logfile, 0, '')
-   o2tf.printlog('dirlen = (%s)' % dirlen, logfile, 0, '')
-   o2tf.printlog('nodelist = (%s)' % nodelist, logfile, 0, '')
-   o2tf.printlog('nodelen = (%s)' % nodelen, logfile, 0, '')
-   o2tf.printlog('logfile = (%s)' % logfile, logfile, 0, '')
-   o2tf.printlog('tarfile = (%s)' % tarfile, logfile, 0, '')
-   o2tf.printlog('buildcmd = (%s)' % buildcmd, logfile, 0, '')
-o2tf.StartMPI(DEBUGON, options.nodelist, logfile)
-o2tf.mpi_runparts( DEBUGON, config.NPROC, str('%s -d %s -c %s -l %s -n %s -t %s' % \
+	o2tf.lamexec(DEBUGON, nproc, str('%s -c -d %s -l %s' % \
 			(buildcmd, 
 			options.dirlist, 
-			options.count, 
-			options.logfile, 
-			options.nodelist, 
-			tarfile) ), 
+			options.logfile) ),
 			options.nodelist, 
 			options.logfile )
+#
+	o2tf.lamexec(DEBUGON, nproc, str('%s -e -d %s -l %s -t %s' % \
+			(buildcmd, 
+			options.dirlist, 
+			options.logfile,
+			tarfile) ),
+			options.nodelist, 
+			options.logfile )
+#
+Usage = 'Usage: %prog [-c|--count count] \
+[-d|--directorylist dirlist] \
+[-h|--help] \
+[-i|--initialize] \
+[-l|-logfile logfilename] \
+[-n|--nodes nodelist] \
+[-t|--tarfile fullpath tar filename] \
+[-u|--user username]'
+#
+if __name__=='__main__':
+	parser = optparse.OptionParser(Usage)
+#
+	parser.add_option('-c', 
+		'--count', 
+		dest='count', 
+		default='1',
+		type='int', 
+		help='Number of times it will build the kernel.  Default = 1.')
+#
+	parser.add_option('-u', 
+		'--user', 
+		dest='userid',
+		type='string', 
+		help='Userid used to open ssh connections.')
+#
+	parser.add_option('-d', 
+		'--directorylist', 
+		dest='dirlist',
+		type='string',
+		help='List of directories that will be used by the test.')
+#
+	parser.add_option('-i',
+		'--initialize', 
+		action="store_true",
+		dest='initialize',
+		default=False,
+		help='Initialize directories before each run. Default=False.')
+#
+	parser.add_option('-l', 
+		'--logfile', 
+		dest='logfile',
+		default='%s/log/run_buildkernel.log' % config.O2TDIR,
+		type='string', 
+		help='If logfile is specified, a single logfile will be used by \
+			all processes, otherwise, individual logfiles will be created per \
+			process. Default will be to create a logfile per process.')
+#
+	parser.add_option('-n', 
+		'--nodes', 
+		dest='nodelist',
+		type='string',
+		help='List of nodes where the test will be executed.')
+#
+	parser.add_option('-t', 
+		'--tarfile', 
+		dest='tarfile', 
+		type='string', 
+		help='Fullpath filename of the tar file containing the kernel that \
+			will be used.')
+
+	(options, args) = parser.parse_args()
+	if len(args) != 0:
+		parser.error('incorrect number of arguments')
+	if not options.tarfile:
+		parser.error('Must provide a gzipped kernel tarball to run the test.')
+	dirlist = options.dirlist.split(',')
+	dirlen = len(dirlist)
+	nodelist = options.nodelist.split(',')
+	nodelen = len(nodelist)
+	if nodelen == 1:
+		nodelist = nodelist.add(options.nodelist)
+	else:
+		nodelist = options.nodelist.split(',')
+	logfile = options.logfile
+	tarfile = options.tarfile
+	if nodelen > config.NPROC:
+	   nproc = nodelen
+	else:
+		nproc = config.NPROC
+#
+if DEBUGON:
+	buildcmd= config.BINDIR+'/buildkernel.py -D'
+else:
+	buildcmd= config.BINDIR+'/buildkernel.py'
+#
+if DEBUGON:
+   o2tf.printlog('run_buildkernel: dirlist = (%s)' % dirlist, logfile, 0, '')
+   o2tf.printlog('run_buildkernel: dirlen = (%s)' % dirlen, logfile, 0, '')
+   o2tf.printlog('run_buildkernel: nodelist = (%s)' % nodelist, logfile, 0, '')
+   o2tf.printlog('run_buildkernel: nodelen = (%s)' % nodelen, logfile, 0, '')
+   o2tf.printlog('run_buildkernel: logfile = (%s)' % logfile, logfile, 0, '')
+   o2tf.printlog('run_buildkernel: tarfile = (%s)' % tarfile, logfile, 0, '')
+   o2tf.printlog('run_buildkernel: buildcmd = (%s)' % buildcmd, logfile, 0, '')
+#
+
+o2tf.StartMPI(DEBUGON, options.nodelist, logfile)
+#
+for i in range(options.count):
+	r = i+1
+	o2tf.printlog('run_buildkernel: Starting RUN# %s of %s' % (r, options.count),
+		logfile,
+		3,
+		'=')
+#
+	if options.initialize or i == 0:
+		Initialize()
+#
+	o2tf.lamexec(DEBUGON, nproc, str('%s -d %s -l %s -n %s' % \
+			(buildcmd, 
+			options.dirlist, 
+			options.logfile, 
+			options.nodelist) ), 
+			options.nodelist, 
+			options.logfile )
+o2tf.printlog('run_buildkernel: Test completed successfully.',
+	logfile,
+	3,
+	'=')
