93be9be2b6574a896a430cf2587742a487422246
Marcos Matsunaga <marcos.matsunaga@oracle.com>
2007-10-17 19:40:30 +0000

    - Made changes to burn-in.sh script to use sudo to mount/umount partitions. Also added a function to obtain information from the ocfs2 partition. Added options to get size, directory, iterations and number of executions. - Added options to allow the script to control number of times a test will be executed. - Added umount/mount calls to make sure it will clear the cache between fill_holes and verify_holes. - Added option to accept a directory where the testfiles will be created.
    Changed files:
    
    M    fill_verify_holes/burn-in.sh
    
    Signed-off-by: Sunil Mushran <sunil.mushran@oracle.com>
    
    Original-revision: 146

--- programs/fill_verify_holes/burn-in.sh
+++ programs/fill_verify_holes/burn-in.sh
@@ -15,8 +15,21 @@ run_fill() {
     size="$3";
     logfile="$4"
 
-    log_run "$BINPATH/fill_holes" $MMAPOPT -f -o "$logfile" -i "$iter" "$filename" "$size"
-    log_run "$BINPATH/verify_holes" "$logfile" "$filename"
+    echo "Creating file..."
+    log_run "${BINPATH}/fill_holes" ${MMAPOPT} -f -o "${logfile}" -i "${iter}" \
+	    "${filename}" "${size}"
+    sleep 10
+    sudo /sbin/fuser -km ${MOUNTPOINT}
+    sleep 10
+    echo "umounting partition"
+    sudo umount ${MOUNTPOINT}
+    sleep 10
+    echo "mounting partition"
+    sudo mount LABEL=${LABEL} ${MOUNTPOINT}
+    sleep 10
+    echo "Verifying..."
+    log_run "${BINPATH}/verify_holes" "-v" "${logfile}" "${filename}"
+    
 
     RET=$?
     if [ $RET -ne 0 ]; then
@@ -28,35 +41,74 @@ run100() {
     iter="$1"
     size="$2"
 
-    fnamebase="iter$iter.size$size"
+    fnamebase="iter${iter}.size${size}"
 
-    for i in `seq -w 0 99`
+    for i in `seq -w 0 ${COUNT}`
     do
-      f="$fnamebase.$i.txt"
-      l="$LOGPATH/$fnamebase.$i.log"
-      run_fill "$iter" "$f" "$size" "$l"
+      f="${DIRECTORY}/${fnamebase}.${i}.txt"
+      l="${LOGPATH}/${fnamebase}.${i}.log"
+      run_fill "${iter}" "${f}" "${size}" "${l}"
     done
+}
+
+#
+# GetDevInfo
+#
+GetDevInfo()
+{
+line=`${DF} -h ${DIRECTORY} | ${GREP} -v Filesystem`
+DEVICE=`echo ${line} | ${AWK} -F" " '{print $1}'`
+MOUNTPOINT=`echo ${line} | ${AWK} -F" " '{print $6}'`
+if [ ${MOUNTPOINT} == "/" -o  ${MOUNTPOINT} == "" ]; then
+	${ECHO} "Specified partition is not mounted.\n"
+	${ECHO} "Aborting....\n"
+	exit 1;
+fi;
+CLUSTERSIZE_BITS=`${DEBUGFS_BIN} -R stats ${DEVICE} | grep Bits|\
+	${AWK} -F" " '{print $8}'`;
+BLOCKSIZE_BITS=`${DEBUGFS_BIN} -R stats ${DEVICE} | grep Bits|\
+	${AWK} -F" " '{print $4}'`;
+LABEL=`${DEBUGFS_BIN} -R stats ${DEVICE} | grep Label:|\
+	${AWK} -F" " '{print $2}'`;
+SLOTS=`${DEBUGFS_BIN} -R stats ${DEVICE} | grep Slots:|\
+	${AWK} -F" " '{print $4}'`;
+CLUSTERSIZE=`echo 2^${CLUSTERSIZE_BITS} |bc`;
+BLOCKSIZE=`echo 2^${BLOCKSIZE_BITS} |bc`;
+#
 }
 
 USAGE=""
 OPTIND=1
-while getopts "mb:l:h?" args
+COUNT=10
+SIZE=1000000
+SUDO="/usr/bin/sudo -u root"
+DEBUGFS_BIN="/usr/bin/sudo -u root /sbin/debugfs.ocfs2"
+MKFS_BIN="/usr/bin/sudo -u root /sbin/mkfs.ocfs2"
+GREP="/bin/grep"
+DF="/bin/df"
+ECHO="/bin/echo -e"
+AWK="/bin/awk"
+#
+while getopts "c:d:i:mb:l:s:h?" args
 do
   case "$args" in
+    c) COUNT="$OPTARG";;
+    d) DIRECTORY="$OPTARG";;
+    i) ITER="$OPTARG";;
     m) MMAPOPT="-m";;
     b) BINPATH="$OPTARG";;
     l) LOGPATH="$OPTARG";;
+    s) SIZE="$OPTARG";;
     h) USAGE="yes";;
     ?) USAGE="yes";;
   esac
 done
 
-if [ -n "$USAGE" ]; then
-    echo "usage: burn-in.sh [ -b path-to-binaries ] [ -l path-for-logfiles ]";
+if [ -n "${USAGE}" ]; then
+    echo "usage: burn-in.sh [ -b path-to-binaries ] [ -l path-for-logfiles ] \
+    	[ -c count ] [ -d  directory ] [ -i iteractions ] [ -s size ]";
     exit 0;
 fi
 
-run100 100 1000000
-run100 1000 1000000
-run100 1000 10000000
-run100 10000 10000000
+GetDevInfo;
+run100 ${ITER} ${SIZE} 
