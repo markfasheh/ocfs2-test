79a80d408fc1544033b6f0acddfe3e7caf2d680e
Junxiao Bi <junxiao.bi@oracle.com>
2015-09-14 10:44:25 +0800

    backup super test: make test fast
    This test run over 10 hours in my env, remove some tests for special cluster
    size and block size to make the test fast.
    
    Signed-off-by: Junxiao Bi <junxiao.bi@oracle.com>
    Reviewed-by: Eric Ren <zren@suse.com>

--- programs/backup_super/test_backup_super.sh
+++ programs/backup_super/test_backup_super.sh
@@ -376,10 +376,10 @@ function volume_small_test()
 ##################################
 function normal_test()
 {
-	for blocksize in 512 1024 2048 4096
+	for blocksize in 512 4096
 	do
 		for clustersize in \
-			4096 8192 16384 32768 65536 131072 262144 524288 1048576
+			4096 32768 1048576
 		do
 
 			vol_byte_size=$FIRST_BACKUP_OFF
